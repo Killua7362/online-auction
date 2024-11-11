f4d66eed340c61d64051d55ce213a2f929abae89
Killua7362 <bhat7362@gmail.com>
2024-11-11 20:22:14 +0530

    postgres+drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  verbose: true,
+});
